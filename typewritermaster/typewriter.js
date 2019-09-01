1a35170632d436f8614015aae8f948a295bd9314
Mateusz  Mieciek <Jak@Mateuszs-MacBook-Pro.local>
2019-09-01 11:49:59 +0200

    random time and hidden letter

--- typewritermaster/typewriter.js
+++ typewritermaster/typewriter.js
@@ -3,23 +3,36 @@
 const text = document.getElementById("typewriter").textContent;
 const arr = text.split("");
 let step = 0;
+let rando = 0;
 const audio = new Audio("typekey1.mp3");
 
-function start(counter) {
-  if (counter < 44) {
+//function that writes one letter at a time from the array
+document.getElementById("typewriter").textContent = " ";
+function type(counter) {
+  if (step <= 42) {
     setTimeout(function() {
       let letter = arr[step];
+
       document.querySelector("h1").append(letter);
+
+      //audio.play();
+
+      rando = Math.floor(Math.random() * 200) + 300;
+      type(counter);
+
       step++;
       counter++;
-      audio.play();
-
-      console.log(counter);
+      //console.log(counter);
+      //console.log(rando);
       console.log(step);
-      console.log(letter);
-
-      start(counter);
-    }, 300);
+    }, rando);
+  } else {
+    console.log("sorry11111");
   }
 }
-start(0);
+
+// start animation
+window.addEventListener("click", event => {
+  type(0);
+  arr[0];
+});
